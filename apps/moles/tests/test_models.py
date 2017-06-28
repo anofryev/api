24103e2470446504da6d67886b1cdca5400ff85e
Ilya Beda <ir4y.ix@gmail.com>
2017-06-28 12:01:57 +0700

    Fix MoleImageTest

--- apps/moles/tests/test_models.py
+++ apps/moles/tests/test_models.py
@@ -1,13 +1,20 @@
-from django.test import TestCase, mock
+from django.test import TransactionTestCase, mock
 
 from apps.main.tests import patch
 from apps.main.tests.mixins import FileTestMixin
 from ..factories import MoleImageFactory
 
 
-class MoleImageTest(FileTestMixin, TestCase):
-    @patch('apps.moles.tasks.requests')
+class MoleImageTest(FileTestMixin, TransactionTestCase):
+    @patch('apps.moles.tasks.requests', new_callable=mock.MagicMock)
     def test_set_up(self, mock_requests):
+        post_mock = mock.MagicMock()
+        post_mock.json.return_value = {
+            'status': 'success',
+            'probability': 0.567,
+            'prediction': 'Seems benign',
+        }
+        mock_requests.post.return_value = post_mock
         with self.fake_media():
             MoleImageFactory.create(photo=self.get_sample_image_file())
 
