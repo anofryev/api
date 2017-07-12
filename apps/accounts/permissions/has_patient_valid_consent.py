97516dbd596e21487aaf9bba97367c6a02708893
Vadim Laletin <rusmiligamer@gmail.com>
2017-07-12 14:44:49 +0700

    Refactor HasPatientValidConsent

--- apps/accounts/permissions/has_patient_valid_consent.py
+++ apps/accounts/permissions/has_patient_valid_consent.py
@@ -1,13 +1,9 @@
 from rest_framework.permissions import BasePermission
 
-from ..models import Patient
+from ..models import PatientConsent
 
 
 class HasPatientValidConsent(BasePermission):
     def has_permission(self, request, view):
-        try:
-            patient = Patient.objects.get(pk=view.kwargs['patient_pk'])
-        except Patient.DoesNotExist:
-            return False
-
-        return patient.valid_consent is not None
+        return PatientConsent.objects.filter_valid().filter(
+            patient=view.kwargs['patient_pk']).exists()
