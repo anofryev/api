f9cd03552c87e9c4c256df9e7c9c9f5654984593
Ilya Beda <ir4y.ix@gmail.com>
2017-08-25 16:27:39 +0700

    Ref #30 Add my_coordinator field to doctor's admin page

--- apps/accounts/admin/doctor.py
+++ apps/accounts/admin/doctor.py
@@ -12,9 +12,18 @@ class DoctorAdmin(UserAdmin):
     fieldsets = (
         (None, {'fields': ('email', 'password')}),
         ('Personal info', {'fields': ('first_name', 'last_name',
-                                        'photo', 'department', 'degree',)}),
-        ('Settings', {'fields': ('units_of_length',)}),
+                                      'photo', 'department', 'degree',)}),
+        ('Settings', {'fields': ('units_of_length', 'my_coordinator',)}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',
                                     'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_active', )}),
     )
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super(DoctorAdmin, self).get_form(request, obj, **kwargs)
+        qs = form.base_fields['my_coordinator'].queryset
+        form.base_fields['my_coordinator'].queryset = qs.select_related(
+            'doctor_ptr')
+        form.base_fields['my_coordinator'].label_from_instance = \
+            lambda obj: obj.doctor_ptr.__str__()
+        return form
