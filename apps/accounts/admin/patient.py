f288849b61eaea90632b7e57c72578a5a0e9ae40
Ilya Beda <ir4y.ix@gmail.com>
2017-09-07 15:06:33 +0300

    Ref #30 show encrypted fields

--- apps/accounts/admin/patient.py
+++ apps/accounts/admin/patient.py
@@ -3,7 +3,10 @@ from django.contrib import admin
 
 class PatientAdmin(admin.ModelAdmin):
     list_display = ('pk', 'sex', 'race', )
+    readonly_fields = ('first_name', 'last_name', 'date_of_birth', 'mrn', )
     fieldsets = (
         ('Public info', {'fields': (
             'photo', 'sex', 'race', )}),
+        ('Encrypted data', {'fields': (
+            'first_name', 'last_name', 'date_of_birth', 'mrn', )})
     )
