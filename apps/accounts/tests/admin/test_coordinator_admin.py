980b12fb36072a016cce87e7a6abbae721f06d5d
Ilya Beda <ir4y.ix@gmail.com>
2017-09-19 13:22:33 +0300

    Ref #34 fix tests

--- apps/accounts/tests/admin/test_coordinator_admin.py
+++ apps/accounts/tests/admin/test_coordinator_admin.py
@@ -51,5 +51,8 @@ class CoordinatorAdminTest(TestCase):
             urlresolvers.reverse(
                 'admin:accounts_coordinator_change', args=[coordinator.pk]))
 
+        fields = response.context['adminform'].form.fields
         self.assertEqual(
-            len(response.context['adminform'].form.fields.values()), 0)
+            len(fields.values()), 1)
+
+        self.assertIsNotNone(fields['site'])
