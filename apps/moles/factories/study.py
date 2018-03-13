82eb176526f3b8e2c61721deb082f8d9987c7f5b
Pavel Rozhkov <prozskov@gmail.com>
2018-03-13 17:03:24 +0700

    Create StudyFactory

--- apps/moles/factories/study.py
+++ apps/moles/factories/study.py
@@ -1,8 +1,27 @@
-from factory import DjangoModelFactory
+from factory import DjangoModelFactory, SubFactory, fuzzy
 
 from ..models import ConsentDoc
+from apps.moles.models.study import Study, StudyToPatient
+from apps.accounts.factories import PatientFactory, DoctorFactory
 
 
 class ConsentDocFactory(DjangoModelFactory):
     class Meta:
         model = ConsentDoc
+
+
+class StudyFactory(DjangoModelFactory):
+    class Meta:
+        model = Study
+
+    title = fuzzy.FuzzyText()
+
+    @classmethod
+    def create(cls, **kwargs):
+        patient = PatientFactory.create()
+        study = super(StudyFactory, cls).create(**kwargs)
+        StudyToPatient.objects.create(
+            study=study,
+            patient=patient
+        )
+        return study
