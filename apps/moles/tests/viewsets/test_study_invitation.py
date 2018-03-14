b364c2d2c9a73c6f31ac050b894da6c2288db296
Pavel Rozhkov <prozskov@gmail.com>
2018-03-14 19:30:22 +0700

    Write tests for StudyInvitation

--- apps/moles/tests/viewsets/test_study_invitation.py
+++ apps/moles/tests/viewsets/test_study_invitation.py
@@ -1,18 +1,59 @@
+import json
+
 from apps.main.tests import APITestCase
+from apps.moles.models.study_invitation import StudyInvitation, \
+    StudyInvitationStatus
+from apps.accounts.models.doctor import Doctor
+from apps.accounts.factories.doctor import DoctorFactory
+from apps.accounts.factories.participant import ParticipantFactory
+from apps.moles.factories.study_invitation import StudyInvitationFactory
 
 
 class StudyInvitationViewSetTest(APITestCase):
     def setUp(self):
         super(StudyInvitationViewSetTest, self).setUp()
 
+    def get_post_data(self, ek):
+        return {
+            'encryption_keys': ek
+        }
+
     def test_list(self):
-        # TODO
-        pass
+        StudyInvitationFactory.create()
+        StudyInvitationFactory.create()
+        self.assertEqual(len(StudyInvitation.objects.all()), 2)
 
     def test_approve(self):
-        # TODO
         pass
+        # TODO: discuss this test with Sasha P.
+        # doctor = DoctorFactory.create()
+        # participant = DoctorFactory.create()
+        # ParticipantFactory.create(doctor_ptr=participant)
+        #
+        # study_invitation = StudyInvitationFactory.create(
+        #     email=participant.email,
+        #     doctor=doctor)
+        # encryption_keys = {study_invitation.doctor.pk: 'qwertyuiop'}
+        # self.authenticate_as_doctor(doctor=participant)
+        # resp = self.client.post('/api/v1/study/invites/{0}/approve/'
+        #                  .format(study_invitation.pk),
+        #                  self.get_post_data(ek=encryption_keys),
+        #                  format='json')
+        # self.assertSuccessResponse(resp)
+        # self.assertEqual(StudyInvitation.objects.all().first().status,
+        #                  StudyInvitationStatus.ACCEPTED)
 
     def test_decline(self):
-        # TODO
-        pass
+        doctor = DoctorFactory.create()
+        participant = DoctorFactory.create()
+        ParticipantFactory.create(doctor_ptr=participant)
+        study_invitation = StudyInvitationFactory.create(
+            email=participant.email,
+            doctor=doctor)
+        self.authenticate_as_doctor(doctor=participant)
+        resp = self.client.post('/api/v1/study/invites/{0}/decline/'
+                                .format(study_invitation.pk),
+                                format='json')
+        self.assertSuccessResponse(resp)
+        self.assertEqual(StudyInvitation.objects.all().first().status,
+                         StudyInvitationStatus.DECLINED)
