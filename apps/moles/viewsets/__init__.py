e42b8d07e42ad69ddbaa6db38572eaa84bd5a608
Pavel Rozhkov <prozskov@gmail.com>
2018-03-14 19:32:43 +0700

    Add StudyInvitationViewSet to viewsets init file

--- apps/moles/viewsets/__init__.py
+++ apps/moles/viewsets/__init__.py
@@ -2,3 +2,4 @@ from .patient_anatomical_site import PatientAnatomicalSiteViewSet
 from .mole import MoleViewSet
 from .mole_image import MoleImageViewSet
 from .study import StudyViewSet, ConsentDocViewSet
+from .study_invitation import StudyInvitationViewSet
