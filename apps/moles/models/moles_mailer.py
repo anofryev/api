7ad2dd6ad277d43aad0e6032996a082ff840e980
Pavel Rozhkov <prozskov@gmail.com>
2018-03-16 10:54:50 +0700

    Remove GetProtocolFromSettingsMixin from moles mailer file

--- apps/moles/models/moles_mailer.py
+++ apps/moles/models/moles_mailer.py
@@ -3,16 +3,7 @@ from django.conf import settings
 from templated_mail.mail import BaseEmailMessage
 
 
-class GetProtocolFromSettingsMixin:
-    def get_context_data(self):
-        context = super(GetProtocolFromSettingsMixin,
-                        self).get_context_data()
-        context['protocol'] = settings.PROTOCOL
-        return context
-
-
-class AddParticipantNotification(GetProtocolFromSettingsMixin,
-                                          BaseEmailMessage):
+class AddParticipantNotification(BaseEmailMessage):
     template_name = 'email/participant_notification.html'
 
     def get_context_data(self):
