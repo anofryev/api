f60ad490ae21308a3394ecfa2c9095263e775cf2
Alexandr Polev <aleksandr.p@pieoneers.com>
2018-03-28 14:20:33 +0700

    extend StudyInvitationSerializer with full doctor and study

--- apps/moles/serializers/study_invitation.py
+++ apps/moles/serializers/study_invitation.py
@@ -1,9 +1,14 @@
 from rest_framework import serializers
 
+from apps.accounts.serializers import DoctorSerializer
+from .study import StudyListSerializer
 from ..models import StudyInvitation
 
 
 class StudyInvitationSerializer(serializers.ModelSerializer):
+    doctor = DoctorSerializer()
+    study = StudyListSerializer()
+
     class Meta:
         model = StudyInvitation
         fields = ('pk', 'email', 'study', 'doctor', 'status')
