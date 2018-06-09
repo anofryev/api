e824c876cc6d02548b8a8e200a60416c5239c4f8
Pavel Rozhkov <prozskov@gmail.com>
2018-06-09 15:24:18 +0700

    Add email send for doctors and participant in invalidate consent method

--- apps/moles/models/study.py
+++ apps/moles/models/study.py
@@ -1,8 +1,10 @@
 from django.db import models
 from django.db.models.signals import m2m_changed
 from django.utils import timezone
+from templated_mail.mail import BaseEmailMessage
 
 from apps.accounts.models import Coordinator
+from apps.accounts.models.participant import get_participant_doctor
 from apps.main.models.mixins.thumbnail import ThumbnailMixin
 from apps.main.storages import private_storage
 from apps.moles.models.upload_paths import study_consent_docs_path
@@ -37,10 +39,30 @@ class Study(models.Model):
         return self.title
 
     def invalidate_consents(self):
+        # TODO: 1. Add send email as celery task
+        # TODO: 2. Add attachment to email
+        context_var = {
+            'study_title': self.title,
+            'study_date_of_change': timezone.now().strftime('%m/%d/%Y %H:%M'),
+            'study_coordinator': self.author.doctor_ptr.get_full_name(),
+            'study_coordinator_email': self.author.doctor_ptr.email
+        }
+
         for study_to_patient in StudyToPatient.objects.filter(study=self):
             if study_to_patient.patient_consent:
                 study_to_patient.patient_consent.date_expired = timezone.now()
                 study_to_patient.patient_consent.save()
+                participant = get_participant_doctor(study_to_patient.patient)
+                if participant:
+                    ParticipantNotificationDocConsentUpdate(
+                        context=context_var).send([participant.email])
+
+        for doctor in self.doctors.all():
+            doctor_context = context_var
+            doctor_context.update({'username': doctor.username})
+            DoctorNotificationDocConsentUdate(context=doctor_context)\
+                .send([doctor.email])
+
 
     class Meta:
         verbose_name = 'Study'
@@ -82,3 +104,11 @@ class StudyToPatient(models.Model):
 
     def __str__(self):
         return '%s - %s' % (self.study, self.patient)
+
+
+class DoctorNotificationDocConsentUdate(BaseEmailMessage):
+    template_name = 'email/doctor_notification_doc_consent_update.html'
+
+
+class ParticipantNotificationDocConsentUpdate(BaseEmailMessage):
+    template_name = 'email/participant_notification_doc_consent_update.html'
