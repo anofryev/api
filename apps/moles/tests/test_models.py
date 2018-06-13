c55af7c997294c96c5adff59b884789be5a89337
Alexandr Polev <aleksandr.p@pieoneers.com>
2018-06-13 11:47:56 +0700

    lets increase coverage

--- apps/moles/tests/test_models.py
+++ apps/moles/tests/test_models.py
@@ -33,7 +33,9 @@ class StudyTest(TestCase):
     def setUp(self):
         self.doctor = DoctorFactory.create()
         self.patient = PatientFactory.create()
-        self.study = StudyFactory.create()
+        self.study = StudyFactory.create(
+            author=CoordinatorFactory.create()
+        )
         self.study.doctors.add(self.doctor)
         self.study.save()
 
@@ -58,10 +60,9 @@ class StudyTest(TestCase):
 
     @patch('apps.moles.models.study.DoctorNotificationDocConsentUpdate.send')
     @patch('apps.moles.models.study.ParticipantNotificationDocConsentUpdate.send')
-    def test_update_consent_docs(self, mock_participant_notification,
+    def test_update_consent_docs(self,
+                                 mock_participant_notification,
                                  mock_doc_notification):
-        self.study.author = CoordinatorFactory.create()
-        self.study.save()
         self.assertTrue(self.study_to_patient.patient_consent.is_valid())
 
         # Make out doctor participant (self.doctor in doctors and in patients)
@@ -79,9 +80,11 @@ class StudyTest(TestCase):
         self.assertTrue(mock_doc_notification.called)
         self.assertTrue(mock_participant_notification.called)
 
-    @patch('apps.moles.models.study.Study.invalidate_consents')
-    def test_update_consent_docs_without_consent(
-            self, mock_invalidate_consents):
+    @patch('apps.moles.models.study.DoctorNotificationDocConsentUpdate.send')
+    @patch('apps.moles.models.study.ParticipantNotificationDocConsentUpdate.send')
+    def test_update_consent_docs_without_consent(self,
+                                                 mock_participant_notification,
+                                                 mock_doc_notification):
         self.assertTrue(self.study_to_patient.patient_consent.is_valid())
 
         self.study_to_patient.patient_consent = None
@@ -91,4 +94,20 @@ class StudyTest(TestCase):
         self.study.consent_docs.add(new_doc)
         self.study.save()
 
-        self.assertTrue(mock_invalidate_consents.called)
+        self.assertTrue(mock_doc_notification.called)
+        self.assertFalse(mock_participant_notification.called)
+
+    @patch('apps.moles.models.study.DoctorNotificationDocConsentUpdate.send')
+    @patch('apps.moles.models.study.ParticipantNotificationDocConsentUpdate.send')
+    def test_update_consent_docs_without_participant_doctor(
+            self,
+            mock_participant_notification,
+            mock_doc_notification):
+        self.assertTrue(self.study_to_patient.patient_consent.is_valid())
+
+        new_doc = ConsentDocFactory.create()
+        self.study.consent_docs.add(new_doc)
+        self.study.save()
+
+        self.assertTrue(mock_doc_notification.called)
+        self.assertFalse(mock_participant_notification.called)
